69c22b19c077d6e8f6eb640cff18ea1e1dbc517e
Paul Halliday <hello@paulhalliday.io>
2021-07-27 14:37:16 +0100

    Added tests

--- test/model_test.dart
+++ test/model_test.dart
@@ -2,13 +2,62 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:liquid_simulation/temperature_model.dart';
 
 void main() {
-  test('initial value is 19', () {
-    final model = TemperatureModel();
-    expect(model.temperature, 19);
+  const temp = 19;
+  const step = 5;
+
+  late TemperatureModel model;
+
+  setUp(() {
+    // Runs once per test
+    model = TemperatureModel();
   });
 
-  test('TODO add more tests', () {
-    // TODO
-    fail('not enough tests added');
+  test('initial value is 19', () {
+    expect(model.temperature, temp);
   });
+
+  test('The temperature should increment in 5 degree steps', () {
+    expect(model.temperature, temp);
+
+    model.increment();
+
+    expect(model.temperature, temp + step);
+  });
+  test('The temperature should decrement in 5 degree steps', () {
+    expect(model.temperature, temp);
+
+    model.decrement();
+
+    expect(model.temperature, temp - step);
+  });
+
+  test('When incremented twice, the temperature should have increased by 10', () {
+    expect(model.temperature, temp);
+
+    model.increment();
+    model.increment();
+
+    const expected = temp + (step * 2);
+
+    expect(model.temperature, expected);
+  });
+
+  test('When decremented twice, the temperature should have decreased by 10', () {
+    expect(model.temperature, temp);
+
+    model.decrement();
+    model.decrement();
+
+    const expected = temp - (step * 2);
+
+    expect(model.temperature, expected);
+  });
+
+  test('Determine if notifyListeners is called', () {
+    expect(model.temperature, temp);
+
+    model.increment();
+  });
+
+  // notifyListeners
 }
